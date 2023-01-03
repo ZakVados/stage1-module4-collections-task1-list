2874ae4dc5633f0a75fdaf2dbdcc2f34b8b2175e
ZakVados <c0H7W2c3#mPZD$2Iuc4B0LbnruPGwi>
2023-01-03 19:55:29 +0200

    ArrayListCreator fixed

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -6,13 +6,11 @@ import java.util.List;
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
         ArrayList<String> res = new ArrayList<>();
-        for (int i = 1; i < sourceList.size(); i++) {
-            if ((i%3) == 0) {
+        for (int i = 0; i < sourceList.size(); i++) {
+            if (((i+1) % 3) == 0) {
                 res.add(sourceList.get(i));
                 res.add(sourceList.get(i));
-                continue;
             }
-            res.add(sourceList.get(i));
         }
         return res;
     }
